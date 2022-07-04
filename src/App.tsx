2f59e93b34112c43c46da45852d9104294703b6a
Ania Orwat-Kaczmarska <orwat.ania@gmail.com>
2022-07-04 16:29:47 +0200

    create users list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import {Container}  from '@mui/material';
+import UserList from './containers/UserList/UserList'
 
 function App() {
   return (
     <Container>
         <Header />
+        <UserList/>
     </Container>
   );
 }
