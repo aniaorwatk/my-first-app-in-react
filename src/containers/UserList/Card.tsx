51306378a8b551d077154077b26654f1cae9eedf
Ania Orwat-Kaczmarska <orwat.ania@gmail.com>
2022-07-05 14:21:36 +0200

    create list and card

---
+++ src/containers/UserList/Card.tsx
@@ -0,0 +1,38 @@
+import react, {FC} from 'react';
+import './UserList.css';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+
+interface Person {
+    name: string;
+    email: string;
+    avatar: string;
+}
+
+const Card:React.FC<Person> = (props) => {
+    return (
+        <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+            </ ListItemAvatar>
+            <ListItemText
+                secondary={
+                    <React.Fragment>
+                    <Typography>
+                        {props.name}
+                    </ Typography>
+                    <Typography>
+                        {props.email}
+                    </ Typography>
+                    </ React.Fragment>
+                }
+            />
+        </ListItem>
+    )
+}
+
+export default Card;
--- src/containers/UserList/UserList.tsx
+++ src/containers/UserList/UserList.tsx
@@ -1,40 +1,19 @@
-import react from 'react'
+import react, {FC} from 'react';
+import './UserList.css';
+import Card from './Card';
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Divider from '@mui/material/Divider';
-import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import React from 'react';
-
-
-
+import {Box} from '@mui/material';
 
 export default function AlignItemsList() {
-    return (
-<List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-<ListItem alignItems="flex-start">
-  <ListItemAvatar>
-    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-  </ListItemAvatar>
-  <ListItemText
-    primary="Brunch this weekend?"
-    secondary={
-      <React.Fragment>
-        <Typography
-          sx={{ display: 'inline' }}
-          component="span"
-          variant="body2"
-          color="text.primary"
-        >
-          Ali Connors
-        </Typography>
-        {" — I'll be in your neighborhood doing errands this…"}
-      </React.Fragment>
-    }
-  />
-</ListItem>
-</List>
-)
+  return (
+    <Box className="UserList--box">
+      <List  sx={{ width: '100%', maxWidth: 360 }}>
+        <Card 
+          name={'Ania'} 
+          email={'orwat.ania@gmail.com'} 
+          avatar={''} 
+        />
+      </List>
+    </Box>   
+  )
 }
