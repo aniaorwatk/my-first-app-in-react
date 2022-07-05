b7ba20dfe0d88ce953b28ffe08e0d026a42194a0
Ania Orwat-Kaczmarska <orwat.ania@gmail.com>
2022-07-05 13:04:13 +0200

    start create fetch function

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,20 +3,31 @@ import './Sidebar.css'
 import {Box, Typography} from '@mui/material';
 import React from "react";
 
-
+interface ImgShiba{
+    img: string;
+    url: string;
+}
 
 const Sidebar: React.FC = () => {
     const title: string = 'Give a smile to shiba'
     const subtitle: string = 'We prepared cute shiba images to make your day better'
 
-// const[imageShiba, setImageShiba]= React.useState < number | null > (0)
-//     React.useEffect(function () {
-//         console.log("Effect ran")
-//         fetch("http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]")
-//             .then(res => res.json())
-//             .then(data => setImageShiba(data))
-//     }, [imageShiba])  
 
+
+const[imageShiba, setImageShiba ]= React.useState < number | null > (0)
+
+    React.useEffect(function () {
+        
+        fetch("http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]")
+            .then(res => res.json())
+            .then(data => setImageShiba(data.url))
+            .catch(err => console.log("error")
+            )
+    }, [])  
+
+    const getShibaImg = () => {
+        console.log("photo")
+    }
 
     return(
         <>
@@ -27,10 +38,12 @@ const Sidebar: React.FC = () => {
         <Typography variant= "h5" gutterBottom component= "div">
         {subtitle}
         </Typography>
-        <button className="sidebar--button"> Get new shiba image </button>
+        <button onClick={getShibaImg} className="sidebar--button"> Get new shiba image </button>
+   
         </Box>
-      </>
+        </>
     )
 }
 
 export default Sidebar
+
