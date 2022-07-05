c8ae1a11a750806834da02b964ddb50d986e0b0c
Ania Orwat-Kaczmarska <orwat.ania@gmail.com>
2022-07-05 13:49:27 +0200

    fix format

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,22 @@
-import react, {FC} from 'react'
-import {Box, Typography} from '@mui/material'
-import './Header.css'
+import react, {FC} from 'react';
+import {Box, Typography} from '@mui/material';
+import './Header.css';
 
 const Header: FC = () => {
-  const title: string = 'Meet ReqRes users!'
-  const subtitle: string = 'Application created with free ReqRes API'
+  const title: string = 'Meet ReqRes users!';
+  const subtitle: string = 'Application created with free ReqRes API';
     return(
       <>
         <Box className="header--box" >
-        <Typography variant="h4" gutterBottom component="div">
-        {title}
-        </Typography> 
-        <Typography variant="h5" gutterBottom component="div">
-        {subtitle}
-        </Typography>
+          <Typography variant="h4" gutterBottom component="div">
+            {title}
+          </ Typography> 
+          <Typography variant="h5" gutterBottom component="div">
+            {subtitle}
+          </ Typography>
         </Box>
       </>
     )
 }
 
-export default Header
+export default Header;
